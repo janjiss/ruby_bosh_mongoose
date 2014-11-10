fc2e7247b933244c370bbd80aeb8139fa4c26b4c
Janis Miezitis <janjiss@gmail.com>
2014-11-10 09:46:49 +0200

    Workign with internal auth

--- lib/ruby_bosh.rb
+++ lib/ruby_bosh.rb
@@ -2,42 +2,33 @@ require 'rest_client'
 require 'builder'
 require 'rexml/document'
 require 'base64'
-require 'hpricot'
+require 'nokogiri'
+require 'pry'
 
-class RubyBOSH  
+class RubyBOSH
   BOSH_XMLNS    = 'http://jabber.org/protocol/httpbind'
   TLS_XMLNS     = 'urn:ietf:params:xml:ns:xmpp-tls'
   SASL_XMLNS    = 'urn:ietf:params:xml:ns:xmpp-sasl'
   BIND_XMLNS    = 'urn:ietf:params:xml:ns:xmpp-bind'
   SESSION_XMLNS = 'urn:ietf:params:xml:ns:xmpp-session'
   CLIENT_XMLNS  = 'jabber:client'
+  HEADERS       =  {"Content-Type" => "text/xml; charset=utf-8", "Accept" => "text/xml"}
 
   class Error < StandardError; end
-  class Timeout < RubyBOSH::Error; end
+  class TimeoutError < RubyBOSH::Error; end
   class AuthFailed < RubyBOSH::Error; end
   class ConnFailed < RubyBOSH::Error; end
 
-  @@logging = true
-  def self.logging=(value)
-    @@logging = value
-  end
+  attr_accessor :jid, :rid, :sid, :success, :custom_resource
 
-  attr_accessor :jid, :rid, :sid, :success , :custom_resource
-  def initialize(jid, pw, service_url, opts={}) 
+  def initialize(jid:, password:, service_url:, timeout: 10, wait: 5, hold: 1)
     @service_url = service_url
-    # Extract the resource if present
-    split_jid = jid.split("/")
-    @jid = split_jid.first
-    @custom_resource = split_jid.last if split_jid.length > 1
-    @pw = pw
-    @host = @jid.split("@").last
+    @password = password
     @success = false
-    @timeout = opts[:timeout] || 3 #seconds 
-    @headers = {"Content-Type" => "text/xml; charset=utf-8",
-                "Accept" => "text/xml"}
-    @wait    = opts[:wait]   || 5
-    @hold    = opts[:hold]   || 3
-    @window  = opts[:window] || 5
+    @timeout = timeout
+    @wait    = wait
+    @hold    = hold
+    parse_jid(jid)
   end
 
   def success?
@@ -50,7 +41,7 @@ class RubyBOSH
 
   def connect
     initialize_bosh_session
-    if send_auth_request 
+    if send_auth_request
       send_restart_request
       request_resource_binding
       @success = send_session_request
@@ -58,16 +49,14 @@ class RubyBOSH
 
     raise RubyBOSH::AuthFailed, "could not authenticate #{@jid}" unless success?
     @rid += 1 #updates the rid for the next call from the browser
-    
     [@jid, @sid, @rid]
   end
 
   private
-  def initialize_bosh_session 
-    response = deliver(construct_body(:wait => @wait, :to => @host,
-                                      :hold => @hold, :window => @window,
+  def initialize_bosh_session
+    post(construct_body(:wait => @wait, :to => @host,
+                                      :hold => @hold,
                                       "xmpp:version" => '1.0'))
-    parse(response)
   end
 
   def construct_body(params={}, &block)
@@ -75,7 +64,7 @@ class RubyBOSH
 
     builder = Builder::XmlMarkup.new
     parameters = {:rid => @rid, :xmlns => BOSH_XMLNS,
-                  "xmpp:version" => "1.0", 
+                  "xmpp:version" => "1.0",
                   "xmlns:xmpp" => "urn:xmpp:xbosh"}.merge(params)
 
     if block_given?
@@ -85,34 +74,34 @@ class RubyBOSH
     end
   end
 
-  def send_auth_request 
+  def send_auth_request
     request = construct_body(:sid => @sid) do |body|
-      auth_string = "#{@jid}\x00#{@jid.split("@").first.strip}\x00#{@pw}" 
-      body.auth(Base64.encode64(auth_string).gsub(/\s/,''), 
+      auth_string = "#{@jid}\x00#{@jid.split("@").first.strip}\x00#{@password}"
+      body.auth(Base64.encode64(auth_string).gsub(/\s/,''),
                     :xmlns => SASL_XMLNS, :mechanism => 'PLAIN')
     end
 
-    response = deliver(request)
-    response.include?("success")
+    response = post(request)
+    response.include?("sid")
   end
 
   def send_restart_request
-    request = construct_body(:sid => @sid, "xmpp:restart" => true, "xmlns:xmpp" => 'urn:xmpp:xbosh')
-    deliver(request).include?("stream:features")
+    request_body = construct_body(:sid => @sid, "xmpp:restart" => true, "xmlns:xmpp" => 'urn:xmpp:xbosh')
+    response = post(request_body)
+    response.include?("stream:features")
   end
 
   def request_resource_binding
     request = construct_body(:sid => @sid) do |body|
-      body.iq(:id => "bind_#{rand(100000)}", :type => "set", 
+      body.iq(:id => "bind_#{rand(100000)}", :type => "set",
               :xmlns => "jabber:client") do |iq|
         iq.bind(:xmlns => BIND_XMLNS) do |bind|
           bind.resource(resource_name)
         end
       end
     end
-    
-    response = deliver(request)
-    response.include?("<jid>") 
+    response = post(request)
+    response.include?("<jid>")
   end
 
   def send_session_request
@@ -120,64 +109,54 @@ class RubyBOSH
       body.iq(:xmlns => CLIENT_XMLNS, :type => "set",
               :id => "sess_#{rand(100000)}") do |iq|
         iq.session(:xmlns => SESSION_XMLNS)
-      end 
+      end
     end
 
-    response = deliver(request)
-    response.include?("body") 
+    response = post(request)
+    response.include?("body")
   end
 
   def parse(_response)
-    doc = Hpricot(_response.to_s)
-    doc.search("//body").each do |body|
-      @sid = body.attributes["sid"].to_s
-    end
+    puts "RESPONSE!!!!!!!!!!! #{_response}"
+    doc = Nokogiri::XML(_response.to_s)
+    @sid = doc.at_css("body").attributes["sid"].to_s rescue @sid
     _response
   end
 
-  begin
-    require 'system_timer'
-    def deliver(xml)
-      SystemTimer.timeout(@timeout) do 
-        send(xml)
-        recv(RestClient.post(@service_url, xml, @headers))
-      end
-    rescue ::Timeout::Error => e
-      raise RubyBOSH::Timeout, e.message
-    rescue Errno::ECONNREFUSED => e
-      raise RubyBOSH::ConnFailed, "could not connect to #{@host}\n#{e.message}"
-    rescue Exception => e
-      raise RubyBOSH::Error, e.message
+  def post(body)
+    # begin
+    ::Timeout::timeout(@timeout) do
+      log_post(body)
+      response = RestClient.post(@service_url, body, HEADERS)
+      parsed_response = parse(response)
     end
-  rescue LoadError
-    warn "WARNING: using the built-in Timeout class which is known to have issues when used for opening connections. Install the SystemTimer gem if you want to make sure the Redis client will not hang." unless RUBY_VERSION >= "1.9" || RUBY_PLATFORM =~ /java/
-
-    require "timeout"
-    def deliver(xml)
-      Timeout.timeout(@timeout) do 
-        send(xml)
-        recv(RestClient.post(@service_url, xml, @headers))
-      end
-    rescue ::Timeout::Error => e
-      raise RubyBOSH::Timeout, e.message
-    rescue Errno::ECONNREFUSED => e
-      raise RubyBOSH::ConnFailed, "could not connect to #{@host}\n#{e.message}"
-    rescue Exception => e
-      raise RubyBOSH::Error, e.message
-    end
+    # rescue ::Timeout::Error => e
+    #   raise RubyBOSH::TimeoutError, e.message
+    # rescue Errno::ECONNREFUSED => e
+    #   raise RubyBOSH::ConnFailed, "could not connect to #{@host}\n#{e.message}"
+    # rescue Exception => e
+    #   raise RubyBOSH::Error, e.message
+    # end
   end
 
-  def send(msg)
-    puts("Ruby-BOSH - SEND\n[#{now}]: #{msg}") if @@logging; msg
+  def log_post(msg)
+    puts("Ruby-BOSH - SEND\n[#{now}]: #{msg}")
   end
 
-  def recv(msg)
-    puts("Ruby-BOSH - RECV\n[#{now}]: #{msg}") if @logging; msg
+  def log_response(msg)
+    puts("Ruby-BOSH - RECV\n[#{now}]: #{msg}")
   end
 
   private
-  def now 
+  def now
     Time.now.strftime("%a %b %d %H:%M:%S %Y")
+  end
+
+  def parse_jid(jid)
+    split_jid = jid.split("/")
+    @jid = split_jid.first
+    @custom_resource = split_jid.last if split_jid.length > 1
+    @host = @jid.split("@").last
   end
 
   def resource_name
@@ -187,10 +166,4 @@ class RubyBOSH
       @custom_resource
     end
   end
-end
-
-
-if __FILE__ == $0
-  p RubyBOSH.initialize_session(ARGV[0], ARGV[1], 
-      "http://localhost:5280/http-bind")
 end
